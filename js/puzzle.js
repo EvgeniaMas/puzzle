d0da40aa10e08b0458d403eb26cb1c9c93060664
Евгения Масликова < >
2020-12-20 00:50:06 +0300

    fix mobile2

--- js/puzzle.js
+++ js/puzzle.js
@@ -143,19 +143,17 @@ function touchMove(event){
   let height_chunk = current_fill.height/2;
   let touchobj = event.changedTouches[0];
   let dist_horizontal = parseInt(touchobj.clientX) - puzzle_startx ;
-  let dist_vertical = parseInt(touchobj.clientY) - puzzle_starty;
- 
-    // if(window.innerHeight > window.innerWidth){
-    //   console.log("Вертикальное");
-    //    current_fill.style.top =   dist_horizontal + width_chunk + 'px';
-    //    current_fill.style.left = dist_vertical + height_chunk +  'px';
-    // }
-    // else{ 
-    	
+  let dist_vertical = parseInt(touchobj.clientY) - puzzle_starty; 
+  current_fill.classList.add('dragged_image');
+    if(window.innerHeight > window.innerWidth){      
       current_fill.style.left =   dist_horizontal + width_chunk*2 + 'px';
       current_fill.style.top = dist_vertical + height_chunk*2 +  'px';  
-      current_fill.classList.add('dragged_image');
-    // }
+    }
+    else{     	
+      current_fill.style.left =   dist_horizontal + width_chunk*2 + 'px';
+      current_fill.style.top = dist_vertical + height_chunk +  'px';        
+   }
+   
   event.preventDefault();
 }
 
@@ -167,7 +165,10 @@ puzzle_coord_y = coordinates.left;
          let position_x = Math.abs(puzzle_coord_x - blocks_coordinates[i][1]);
          let position_y = Math.abs(puzzle_coord_y - blocks_coordinates[i][0]);
       if(position_x<50 && position_y<50){
-        if(cells[i].classList.contains(current_fill_id)){
+        if(cells[i].classList){
+          if(cells[i].classList.contains(current_fill_id)){     
+
+        
             current_fill.classList.remove('chunk_puzzle');
             current_fill.classList.remove('dragged_image');
               cells[i].appendChild(current_fill);  
@@ -176,6 +177,8 @@ puzzle_coord_y = coordinates.left;
            current_fill.style.top = chunks_coordinate[i][0] + '%';
            current_fill.style.left = chunks_coordinate[i][1] + '%'; 
         }
+
+         }
         else{
           returnPuzzleImage();          
         } 
